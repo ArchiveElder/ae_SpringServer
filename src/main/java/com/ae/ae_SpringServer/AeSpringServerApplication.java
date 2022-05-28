aa4e2926f5a3b408b9465715c9ad21a75841e5b1
rosesua318 <rosesua@duksung.ac.kr>
2022-05-28 20:55:57 +0900

    [Chore] 프로젝트 생성 (김민서)

---
+++ src/main/java/com/ae/ae_SpringServer/AeSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.ae.ae_SpringServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ae/ae_SpringServer/AeSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.ae_SpringServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
