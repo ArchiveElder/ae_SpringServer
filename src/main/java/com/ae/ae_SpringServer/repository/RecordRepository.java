a0a56aa5d193ae2e72efce8f4efcb83724d82725
Vector1331 <tpgus4796@gmail.com>
2022-06-22 20:21:16 +0900

    식단 분석 준비

--- src/main/java/com/ae/ae_SpringServer/repository/RecordRepository.java
+++ src/main/java/com/ae/ae_SpringServer/repository/RecordRepository.java
@@ -35,6 +35,15 @@ public class RecordRepository {
                 .setParameter("date", date)
                 .getResultList();
     }
+    //날짜별로 그룹화하고 정렬후 현재 날짜는 제외하고 7개 날짜 검색하는 쿼리
+    //가장 최근날짜와 가장 오래된 날짜를 변수에 저장하고, 다시 em으로 record 테이블 선택,
+    //user 테이블과 join fetch하며 날짜가 오래된 날짜와 최근날짜 사이인(비교문) 식단을 검색하는 쿼리날리고,
+    // 식단 리스트 값 반환
+    public List<Record> analysis(Long id) {
+        return em.createQuery("select r from Record r join fetch r.user u where u.id = :param", Record.class)
+                .setParameter("param", id)
+                .getResultList();
+    }
 
     /* 2차때 구현
     public List<Record> findUserRecordSome(Long id) {
