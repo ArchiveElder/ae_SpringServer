e58457cf24090bca607299518c7ac7617f9508d6
Vector1331 <tpgus4796@gmail.com>
2022-06-28 16:52:22 +0900

    [Feat] 식단분석 기능 준비 (김세현)

--- src/main/java/com/ae/ae_SpringServer/api/AnalysisApiController.java
+++ src/main/java/com/ae/ae_SpringServer/api/AnalysisApiController.java
@@ -1,11 +1,18 @@
 package com.ae.ae_SpringServer.api;
 
+import com.ae.ae_SpringServer.domain.Record;
 import com.ae.ae_SpringServer.service.AnalysisService;
 import com.ae.ae_SpringServer.service.RecordService;
 import com.ae.ae_SpringServer.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +21,37 @@ public class AnalysisApiController {
     private final UserService userService;
     private final AnalysisService analysisService;
 
-    //@GetMapping("api/analysis")
+    //5-1
+    @GetMapping("api/analysis")
+    public AnalysisResponse analysisResponse() {
+        Long id = Long.valueOf(3);
+        List<Record> findRecords = analysisService.findRecords(id);
+        List<AnalysisDto> collect = findRecords.stream()
+                .map(m -> new AnalysisDto(m.getId(),m.getDate(), m.getCal()))
+                .collect(toList());
+        return new AnalysisResponse(collect);
 
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class AnalysisResponse {
+//        private int ratioCarb;
+//        private int ratioPro;
+//        private int ratioFat;
+//        private int totalCarb;
+//        private int totalPro;
+//        private int totalFat;
+        private List<AnalysisDto> analysisDtos;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class AnalysisDto {
+        private Long id;
+        private String date;
+        //private int totalCal;
+        private String cal;
+    }
 }
--- src/main/java/com/ae/ae_SpringServer/repository/RecordRepository.java
+++ src/main/java/com/ae/ae_SpringServer/repository/RecordRepository.java
@@ -1,10 +1,16 @@
 package com.ae.ae_SpringServer.repository;
 
+import com.ae.ae_SpringServer.api.dto.DateAnalysisDto;
 import com.ae.ae_SpringServer.domain.Record;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -29,14 +35,50 @@ public class RecordRepository {
                 .getResultList();
     }
     //날짜별로 그룹화하고 정렬후 현재 날짜는 제외하고 7개 날짜 검색하는 쿼리
+
+//    public List analysisDates(Long id, int offset, int limit) {
+//        //return em.createQuery("SELECT r.date FROM Record r JOIN FETCH r.user u WHERE u.id = :param GROUP BY r.date ORDER BY r.date DESC LIMIT 7", Record.class)
+//        /*return em.createQuery("SELECT r.date FROM Record r JOIN r.user u WHERE u.id = :param GROUP BY r.date ORDER BY r.date DESC")
+//                .setMaxResults(7)
+//                .setParameter("param", id)
+//                .getResultList();*/
+//        return em.createQuery("SELECT r.date FROM Record r" +
+//                        " JOIN r.user u WHERE u.id = :param" +
+//                        " GROUP BY r.date" +
+//                        " ORDER BY r.date DESC")
+//                .setParameter("param" ,id)
+//                .setFirstResult(offset)
+//                .setMaxResults(limit)
+//                .getResultList();
+//    }
+    public Page analysisDates(Long id, Pageable paging){
+        Query query = em.createQuery("SELECT r.date, SUM(r.cal), SUM(r.carb), SUM(r.protein), SUM(r.fat)" +
+                        " FROM Record r" +
+                        " INNER JOIN r.user u WHERE u.id = :param" +
+                        " GROUP BY r.date" +
+                        " ORDER BY r.date DESC")
+                        .setParameter("param", id);
+
+
+        List<Object[]> resultList = query.getResultList();
+        List<DateAnalysisDto> dateAnalysisDtos = new ArrayList<>();
+        for(Object[] row : resultList){
+            String date = (String)row[0];
+            String sumCal = (String)row[1];
+            String sumCarb = (String)row[2];
+            String sumPro = (String)row[3];
+            String sumFat = (String)row[4];
+            dateAnalysisDtos.add(new DateAnalysisDto(date, sumCal, sumCarb, sumPro, sumFat));
+        }
+        return (Page)dateAnalysisDtos;
+    }
+
     //가장 최근날짜와 가장 오래된 날짜를 변수에 저장하고, 다시 em으로 record 테이블 선택,
     //user 테이블과 join fetch하며 날짜가 오래된 날짜와 최근날짜 사이인(비교문) 식단을 검색하는 쿼리날리고,
     // 식단 리스트 값 반환
-    public List<Record> analysis(Long id) {
-        return em.createQuery("select r from Record r join fetch r.user u where u.id = :param", Record.class)
-                .setParameter("param", id)
-                .getResultList();
-    }
+//    public List<Record> analysis(){
+//
+//    }
 
     public List<Record> findDetaileOne(Long id, Long record_id) {
         return em.createQuery("select r from Record r join r.user u where u.id = :param and r.id = :record_id", Record.class)
@@ -47,5 +89,20 @@ public class RecordRepository {
     }
 
 
+    public List<Record> analysis(String frst, String lst) {
+        return em.createQuery("SELECT r FROM Record r WHERE r.date <= ?1 and r.date >= ?2",Record.class)
+                .setParameter(1,frst)
+                .setParameter(2, lst)
+                .getResultList();
+    }
 
+//    @Data
+//    @AllArgsConstructor
+//    private class DateAnalysisDto {
+//        private String date;
+//        private String sumCal;
+//        private String sumCarb;
+//        private String sumPro;
+//        private String sumFat;
+//    }
 }
