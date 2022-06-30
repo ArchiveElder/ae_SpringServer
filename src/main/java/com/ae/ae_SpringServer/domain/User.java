6b93c5bcf42414b85fd99a612b7d5224fdc32650
Vector1331 <tpgus4796@gmail.com>
2022-06-30 15:46:31 +0900

    [Fix] User 도메인 수정, 불필요 코드 제거 (김세현)

--- src/main/java/com/ae/ae_SpringServer/domain/User.java
+++ src/main/java/com/ae/ae_SpringServer/domain/User.java
@@ -26,51 +26,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
-
-    private String kakao;
-
     private String name;
-    private int icon;
     private int gender;
+    private int age;
     private String height;
     private String weight;
+    private String kakao;
+    private int icon;
     private int activity;
-
-    @Column(name = "user_calory")
-    private int ucalory;
-
-    @Column(name = "user_carb")
-    private int ucarb;
-
-    @Column(name = "user_pro")
-    private int upro;
-
-    @Column(name = "user_fat")
-    private int ufat;
-
-    @Column(name = "remain_calory")
-    private int rcalory;
-
-    @Column(name = "remain_carb")
-    private int rcarb;
-
-    @Column(name = "remain_pro")
-    private int rpro;
-
-    @Column(name = "remain_fat")
-    private int rfat;
 
     @Column(name = "signup_date")
     private String date;
 
     @Column(name = "recommend_calory")
-    private int rec_cal;
+    private int rcal;
+    @Column(name = "recommend_carb")
+    private int rcarb;
+    @Column(name = "recommend_pro")
+    private int rpro;
+    @Column(name = "recommend_fat")
+    private int rfat;
 
-
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "user")
-    private List<Recommend> recommends = new ArrayList<>();
 
     @JsonIgnore
     @OneToMany(mappedBy = "user")
