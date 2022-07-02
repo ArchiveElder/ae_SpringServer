e92de418164faf0c9a7d29713c09617c821effd3
rosesua318 <rosesua@duksung.ac.kr>
2022-07-02 20:41:01 +0900

    [Fix] 회원 수정에서 icon 제외 (김민서)

--- src/main/java/com/ae/ae_SpringServer/dto/UserUpdateRequestDto.java
+++ src/main/java/com/ae/ae_SpringServer/dto/UserUpdateRequestDto.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserUpdateRequestDto {
     private int age;
-    private int icon;
     private String height;
     private String weight;
     private int activity;
