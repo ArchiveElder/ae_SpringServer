bdcb0a195475708bc8610ef745941b6a0af764f8
Vector1331 <tpgus4796@gmail.com>
2022-08-07 23:27:36 +0900

    [Test] 4-1 카카오로그인 테스트 코드 (김세현)

--- src/test/java/com/ae/ae_SpringServer/service/UserServiceTest.java
+++ src/test/java/com/ae/ae_SpringServer/service/UserServiceTest.java
@@ -1,14 +1,31 @@
 package com.ae.ae_SpringServer.service;
 
 import com.ae.ae_SpringServer.domain.User;
+import com.ae.ae_SpringServer.dto.SignupRequestDto;
+import com.ae.ae_SpringServer.dto.UserSocialLoginRequestDto;
 import com.ae.ae_SpringServer.dto.UserUpdateRequestDto;
+import com.nimbusds.jose.shaded.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.EntityManager;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -92,4 +109,40 @@ public class UserServiceTest {
         assertNotEquals(orginRfat,user.getRfat());
 
     }
+
+
+    //4-1 (3-3보다 우선해서 진행)
+    @Test
+    public void 카카오로그인() {
+        //case 1 : db에 있는 Token의 경우
+
+        //given
+        String token = "dgm9YeCAGO5rRxzCESVi9Ids2PDyc2gx5aXs3AY3Cj1ylwAAAYGp5n1p";
+        //UserSocialLoginRequestDto requestDto = new UserSocialLoginRequestDto(accessToken);
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = URI.create("https://kapi.kakao.com/v2/user/me");
+        HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        MultiValueMap<String, Object> pa = new LinkedMultiValueMap<>();
+        HttpEntity<MultiValueMap<String, Object>> restRequest = new HttpEntity<>(pa, headers);
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                HttpStatus statusCode = response.getStatusCode();
+                return statusCode.series() == HttpStatus.Series.SERVER_ERROR;
+            }
+        });
+        ResponseEntity<JSONObject> apiResponse = restTemplate.postForEntity(uri, restRequest, JSONObject.class);
+        JSONObject responseBody = apiResponse.getBody();
+
+        String kakaoId = String.valueOf(responseBody.get("id"));
+
+        //when : 카카오 로그인을 진행했을 때
+        Optional<User> user = userService.findByKakaoId(kakaoId);
+
+        //then
+        assertEquals(kakaoId, user.get().getKakao());
+
+    }
+
 }
