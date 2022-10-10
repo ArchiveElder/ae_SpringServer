72be74a7d66449753665b3810e1a9f9f07b4801b
Vector1331 <tpgus4796@gmail.com>
2022-10-10 22:27:43 +0900

    api 1-1 validation 추가

--- src/main/java/com/ae/ae_SpringServer/api/RecordApiController.java
+++ src/main/java/com/ae/ae_SpringServer/api/RecordApiController.java
@@ -1,5 +1,6 @@
 package com.ae.ae_SpringServer.api;
 
+import com.ae.ae_SpringServer.config.BaseResponse;
 import com.ae.ae_SpringServer.domain.Record;
 import com.ae.ae_SpringServer.domain.User;
 import com.ae.ae_SpringServer.dto.request.DateRecordRequestDto;
@@ -17,10 +18,13 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ae.ae_SpringServer.config.BaseResponseStatus.*;
 import static java.util.stream.Collectors.toList;
 
 @RestController
@@ -32,7 +36,7 @@ public class RecordApiController {
 
         //1-1
     @PostMapping(value = "/api/record", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public RecordResponseDto createRecord(@AuthenticationPrincipal String userId,
+    public  BaseResponse<RecordResponseDto> createRecord(@AuthenticationPrincipal String userId,
                                           @RequestParam (value = "image", required = false) MultipartFile multipartFile,
                                           @RequestParam (value = "text", required = true) String text,
                                           @RequestParam (value = "calory", required = false) String calory,
@@ -44,7 +48,15 @@ public class RecordApiController {
                                           @RequestParam (value = "amount", required = false) Double amount,
                                           @RequestParam (value = "meal", required = true) int meal
                                              ) throws IOException {
+
+
+        if(userId == null) {
+            return new BaseResponse<>(EMPTY_JWT);
+        }
         User user = userService.findOne(Long.valueOf(userId));
+        if (user == null) {
+            return new BaseResponse<>(INVALID_JWT);
+        }
         String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd."));
         //S3 Bucket upload
         String img_url =  "empty";
@@ -53,14 +65,88 @@ public class RecordApiController {
                 img_url = s3Uploader.upload(multipartFile, "static");
             }
         }
+        if(multipartFile == null || multipartFile.isEmpty()) {
+            return new BaseResponse<>(POST_RECORD_NO_IMAGE);
+        }
+        if(text.isEmpty() || text.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_TEXT);
+        }
 
-        Long id = null;
+        if(calory.isEmpty() || calory.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_CALORY);
+        }
+        if(Double.valueOf(calory) <= 0) {
+            return new BaseResponse<>(POST_RECORD_MINUS_CALORY);
+        }
+
+        if(carb.isEmpty() || carb.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_CARB);
+        }
+
+        if(Double.valueOf(carb) <= 0) {
+            return new BaseResponse<>(POST_RECORD_MINUS_CARB);
+        }
+
+        if(protein.isEmpty() || protein.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_PROTEIN);
+        }
+
+        if(Double.valueOf(protein) <= 0) {
+            return new BaseResponse<>(POST_RECORD_MINUS_PROTEIN);
+        }
+
+        if(fat.isEmpty() || fat.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_FAT);
+        }
+
+        if(Double.valueOf(fat) <= 0) {
+            return new BaseResponse<>(POST_RECORD_MINUS_FAT);
+        }
+
+        if(rdate.isEmpty() || rdate.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_RDATE);
+        }
+        try {
+            LocalDate.from(LocalDate.parse(rdate, DateTimeFormatter.ofPattern("yyyy.MM.dd.")));
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            return new BaseResponse<>(POST_RECORD_INVALID_RDATE);
+        }
+
+        if(rtime.isEmpty() || rtime.equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_RTIME);
+        }
+
+        try{
+            LocalTime.from(LocalTime.parse(rtime, DateTimeFormatter.ofPattern("HH:mm")));
+        }catch (DateTimeParseException e) {
+            e.printStackTrace();
+            return new BaseResponse<>(POST_RECORD_INVALID_RTIME);
+        }
+
+
+        if(amount == null) {
+            return new BaseResponse<>(POST_RECORD_NO_AMOUNT);
+        }
+
+        if(amount <= 0) {
+            return new BaseResponse<>(POST_RECORD_MINUS_AMOUNT);
+        }
+
+        if(String.valueOf(meal).isEmpty() || String.valueOf(meal).equals("")) {
+            return new BaseResponse<>(POST_RECORD_NO_MEAL);
+        }
+
+        if(meal != 0 && meal != 1 && meal != 2) {
+            return new BaseResponse<>(POST_RECORD_INVALID_MEAL);
+        }
+
         Record record = Record.createRecord(img_url, text, date, calory, carb, protein, fat,
                 rdate, rtime, amount, meal, user);
 
-        id = recordService.record(record);
+        Long id = recordService.record(record);
 
-        return new RecordResponseDto(id.intValue());
+        return new BaseResponse<>(new RecordResponseDto(id.intValue()));
     }
 
     //1-2
