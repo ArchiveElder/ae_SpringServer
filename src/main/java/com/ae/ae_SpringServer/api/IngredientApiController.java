209dae24295657ac128c609ad488f292b3f427b5
Vector1331 <tpgus4796@gmail.com>
2022-10-12 16:49:16 +0900

    [Refactor] api 9 validation 코드 추가

--- src/main/java/com/ae/ae_SpringServer/api/IngredientApiController.java
+++ src/main/java/com/ae/ae_SpringServer/api/IngredientApiController.java
@@ -1,5 +1,6 @@
 package com.ae.ae_SpringServer.api;
 
+import com.ae.ae_SpringServer.config.BaseResponse;
 import com.ae.ae_SpringServer.domain.Ingredient;
 import com.ae.ae_SpringServer.dto.response.IngredientResponseDto;
 import com.ae.ae_SpringServer.dto.response.ResResponse;
@@ -9,6 +10,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+
+import static com.ae.ae_SpringServer.config.BaseResponseStatus.EMPTY_JWT;
+import static com.ae.ae_SpringServer.config.BaseResponseStatus.INVALID_JWT;
 import static java.util.stream.Collectors.toList;
 
 
@@ -17,13 +21,19 @@ import static java.util.stream.Collectors.toList;
 public class IngredientApiController {
     private final IngredientService ingredientService;
 
-    //9-1
+    //[GET] 9-1 모든 재료 조회
     @GetMapping("/api/ingredient")
-    public ResResponse ingredients(@AuthenticationPrincipal String userId) {
+    public BaseResponse<ResResponse> ingredients(@AuthenticationPrincipal String userId) {
+        if(userId.equals("INVALID JWT")){
+            return new BaseResponse<>(INVALID_JWT);
+        }
+        if(userId == null) {
+            return new BaseResponse<>(EMPTY_JWT);
+        }
         List<Ingredient> findIngredients = ingredientService.findAllIngredients();
         List<IngredientResponseDto> collect = findIngredients.stream()
                 .map(m -> new IngredientResponseDto(m.getId(), m.getName()))
                 .collect(toList());
-        return new ResResponse(collect.size(), collect);
+        return new BaseResponse<>(new ResResponse(collect.size(), collect));
     }
 }
