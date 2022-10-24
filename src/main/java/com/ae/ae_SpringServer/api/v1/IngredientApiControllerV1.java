f6e0264d59250d79cb0ea9795942eaf75e7a0d2c
Vector1331 <tpgus4796@gmail.com>
2022-10-24 22:48:15 +0900

    version1 사용자를 위한 ingredientApiController 코드 보존

--- src/main/java/com/ae/ae_SpringServer/api/v1/IngredientApiControllerV1.java
+++ src/main/java/com/ae/ae_SpringServer/api/v1/IngredientApiControllerV1.java
@@ -1,6 +1,6 @@
-package com.ae.ae_SpringServer.api;
+package com.ae.ae_SpringServer.api.v1;
 
-import com.ae.ae_SpringServer.config.BaseResponse;
+
 import com.ae.ae_SpringServer.domain.Ingredient;
 import com.ae.ae_SpringServer.dto.response.IngredientResponseDto;
 import com.ae.ae_SpringServer.dto.response.ResResponse;
@@ -10,30 +10,21 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-
-import static com.ae.ae_SpringServer.config.BaseResponseStatus.EMPTY_JWT;
-import static com.ae.ae_SpringServer.config.BaseResponseStatus.INVALID_JWT;
 import static java.util.stream.Collectors.toList;
 
 
 @RestController
 @RequiredArgsConstructor
-public class IngredientApiController {
+public class IngredientApiControllerV1 {
     private final IngredientService ingredientService;
 
-    //[GET] 9-1 모든 재료 조회
-    @GetMapping("/api/v2/ingredient")
-    public BaseResponse<ResResponse> ingredients(@AuthenticationPrincipal String userId) {
-        if(userId.equals("INVALID JWT")){
-            return new BaseResponse<>(INVALID_JWT);
-        }
-        if(userId == null) {
-            return new BaseResponse<>(EMPTY_JWT);
-        }
+    //9-1
+    @GetMapping("/api/ingredient")
+    public ResResponse ingredients(@AuthenticationPrincipal String userId) {
         List<Ingredient> findIngredients = ingredientService.findAllIngredients();
         List<IngredientResponseDto> collect = findIngredients.stream()
                 .map(m -> new IngredientResponseDto(m.getId(), m.getName()))
                 .collect(toList());
-        return new BaseResponse<>(new ResResponse(collect.size(), collect));
+        return new ResResponse(collect.size(), collect);
     }
 }
