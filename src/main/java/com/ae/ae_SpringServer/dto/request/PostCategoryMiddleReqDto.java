c9e833f246bea6993ce77ab9a8321aeefa494640
rosesua318 <rosesua@duksung.ac.kr>
2022-11-09 20:28:31 +0900

    [Feat] 음식점 카테고리 검색 기능 추가 (김민서)

---
+++ src/main/java/com/ae/ae_SpringServer/dto/request/PostCategoryMiddleReqDto.java
@@ -0,0 +1,9 @@
+package com.ae.ae_SpringServer.dto.request;
+
+import lombok.Data;
+
+@Data
+public class PostCategoryMiddleReqDto {
+    private String mainCategory;
+    private String middleCategory;
+}
---
+++ src/main/java/com/ae/ae_SpringServer/dto/request/PostSiteWideMainReqDto.java
@@ -0,0 +1,9 @@
+package com.ae.ae_SpringServer.dto.request;
+
+import lombok.Data;
+
+@Data
+public class PostSiteWideMainReqDto {
+    private String siteWide;
+    private String mainCategory;
+}
